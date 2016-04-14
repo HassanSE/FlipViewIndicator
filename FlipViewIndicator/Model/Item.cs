b7e5fd3a030c91d18db90ed0643e33c8872eda6c
unknown <Muhammad.Hassan@PC175.ninealpha.com>
2016-04-14 14:25:15 +0500

    Adds solution to repo

---
+++ FlipViewIndicator/Model/Item.cs
@@ -0,0 +1,42 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace FlipViewIndicator.Model
+{
+    public class Item : ObservableObject
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { Set(() => Name, ref name, value); }
+        }
+
+        private Color color;
+        public Color Color
+        {
+            get { return color; }
+            set { Set(() => Color, ref color, value); this.Brush = new SolidColorBrush(value); }
+        }
+
+        private SolidColorBrush brush;
+        public SolidColorBrush Brush
+        {
+            get { return brush; }
+            set { Set(() => Brush, ref brush, value); }
+        }
+
+        private bool isSelected;
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set { Set(() => IsSelected, ref isSelected, value); }
+        }
+    }
+}
---
+++ FlipViewIndicator/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using FlipViewIndicator.Model;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using GalaSoft.MvvmLight.Command;
+
+namespace FlipViewIndicator.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            this.Colors = new ObservableCollection<Item>(typeof(Colors).GetRuntimeProperties().Take(25).Select
+                (c => new Item
+            {
+                Color = (Windows.UI.Color)c.GetValue(null),
+                Name = c.Name
+            }));
+            this.SelectedColor = Colors[0];
+            IndicatorTapCommand = new RelayCommand<Item>( (s) => OnIndicatorTap(s), (s) => true );
+        }
+
+        private Item selectedColor;
+        public Item SelectedColor
+        {
+            get { return selectedColor; }
+            set
+            {
+                Set(() => SelectedColor, ref selectedColor, value);
+                foreach (var color in this.Colors.Where(x => x.IsSelected))
+                    color.IsSelected = false;
+                if (value != null)
+                    value.IsSelected = true;
+            }
+        }
+
+        private ObservableCollection<Item> color;
+        public ObservableCollection<Item> Colors
+        {
+            get { return color; }
+            set
+            {
+                Set(() => Colors, ref color, value);
+            }
+        }
+
+        public RelayCommand<Item> IndicatorTapCommand { get; private set; }
+
+        void OnIndicatorTap(Item selectedItem)
+        {
+            this.SelectedColor = selectedItem;
+            this.IndicatorTapCommand.RaiseCanExecuteChanged();
+        }
+    }
+}
